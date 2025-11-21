0787cb98287a6c8e830f5e7fcfc3539d964a825f
andreak11 <andreadukh@icloud.com>
2025-11-21 15:27:02 +0100

    Secured Booking API using token authentication (added IsAuthenticated, updated URLs)

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticated
+
 
 def index(request):
     return render(request, 'index.html', {})
@@ -17,3 +19,5 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
+
